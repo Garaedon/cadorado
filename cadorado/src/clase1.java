53366617fa1716c3d1999608997577192e029cae
cadorado <cadorado@106-02.info2>
2012-05-21 13:08:49 +0200

    realizado metodos

--- cadorado/src/clase1.java
+++ cadorado/src/clase1.java
@@ -1,6 +1,10 @@
 
 public class clase1 
 {
+	public void metodo1_clase1()
+	{
+		System.out.println("metodo realizado");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("soy la clase 1");
--- cadorado/src/clase2.java
+++ cadorado/src/clase2.java
@@ -1,6 +1,10 @@
 
 public class clase2 
 {
+	public void metodo1_clase2()
+	{
+		System.out.println("metodo realizado");
+	}
 	public static void main(String[]args)
 	{
 		System.out.println("soy la clase 2");
