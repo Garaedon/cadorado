d3aeb352006b51d4a17e0048869a1f3657a4382b
cadorado <cadorado@106-02.info2>
2012-05-21 13:11:39 +0200

    rama 1 creada

--- cadorado/src/clase1.java
+++ cadorado/src/clase1.java
@@ -1,6 +1,10 @@
 
 public class clase1 
 {
+	public void metodo_rama1_clase1()
+	{
+		System.out.println("rama 1 creada");
+	}
 	public void metodo1_clase1()
 	{
 		System.out.println("metodo realizado");
